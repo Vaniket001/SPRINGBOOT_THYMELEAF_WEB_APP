1649b0185b12da6b7aa612d1a9a86cf0f08f3b4e
Aniket <kumaraniket900@gmail.com>
2023-05-14 19:45:10 +0530

    First Commit

---
+++ src/main/java/com/mvc/sb/SpringbootThymeleafWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.mvc.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootThymeleafWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootThymeleafWebAppApplication.class, args);
+		System.out.println("SpringbootThymeleafWebAppApplication started !!");
+	}
+
+}
